97d28d16a6e4c531b39469f5c5025e22355757a8
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-05 16:23:42 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -49,5 +49,5 @@ createApp(App)
 .use(ElTooltip)
 .use(ElProgress)
 .mount('#app')
-.config.globalProperties.$notify = ElNotification
+config.globalProperties.$notify = ElNotification
 .prototype.$confirm = ElMessageBox.confirm
